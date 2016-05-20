fdf5a39b5eb04897d5ea63a35be1e2d07c2a755c
soroa <soroa@student.ethz.ch>
2016-05-20 17:54:16 +0200

    added test cases folder for divByZeroTest

---
+++ testcases/division_by_zero/At01_dbz.java
@@ -0,0 +1,13 @@
+public class At01_dbz {
+
+	public static void m(int n) {
+
+		PrinterArray p1 = new PrinterArray(10);
+		
+		if (n==0) {
+			p1.sendJob(1/n);
+		}
+		
+	}
+
+}
---
+++ testcases/division_by_zero/At02_ndbz.java
@@ -0,0 +1,14 @@
+
+public class At02_ndbz {
+
+	public static void m(int n) {
+
+		PrinterArray p1 = new PrinterArray(10);
+		
+		if (n==0) {
+			p1.sendJob(n);
+		}
+		
+	}
+
+}
