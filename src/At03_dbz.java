2a3ce8f0a4de48f0a4a12531c5a2d7886ac93955
soroa <soroa@student.ethz.ch>
2016-05-26 13:33:40 +0200

    added some division by zero test cases to default package

--- src/At03_dbz.java
+++ src/At03_dbz.java
@@ -1,14 +1,16 @@
 
-public class At02_ndbz {
+public class At03_dbz {
 
 	public static void m(int n) {
 
 		PrinterArray p1 = new PrinterArray(10);
-		
+		int a; 
 		if (n>0) {
-			p1.sendJob(1/n);
+			a= 1/n;
+			}
+		else {
+			a = 1/(n+1);
 		}
 		
 	}
-
 }
